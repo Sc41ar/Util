a28bf61c943ad85bfa402db8d137890851f78d12
Nikita <mrniki2396@gmail.com>
2024-02-02 20:51:51 +0700

    typeCheck? + test

--- src/main/java/org/example/FileParser.java
+++ src/main/java/org/example/FileParser.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class FileParser {
+
+    //double/float, etc -> float
+//    int, short, byte, etc -> Int
+//    other -> String
+    public String checkType(String originalSt) {
+        try {
+            var intgr = Integer.parseInt(originalSt);
+            return "Int";
+        } catch (NumberFormatException e) {
+            System.out.println("Не целое");
+        }
+        try {
+            var dotsStr = originalSt.replace(',', '.');
+            var dbl = Double.parseDouble(dotsStr);
+            return "Float";
+        } catch (NumberFormatException e) {
+            System.out.println("Не с плавающей");
+        }
+
+        return "String";
+
+    }
 }
